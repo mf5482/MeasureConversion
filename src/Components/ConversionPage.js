9995f81eea11a550864a6705e7742b758d330ab2
Michael Fishler <mfishler1@gmail.com>
2021-06-12 22:43:26 -0400

    Finished common conversions

--- src/Components/ConversionPage.js
+++ src/Components/ConversionPage.js
@@ -3,26 +3,37 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import Table from 'react-bootstrap/table'
 
+const vol = ['1 Gallon = 4 Quarts', '1 Gallon = 3.785 Liters', '1 Quart = 4 cups', '1 cup = 8 Fluid Ounces']
+const len = ['1 Mile = 1.609 Kilometers', '1 mile = 1760 Yards', '1 Yard = 3 Feet', '1 Foot = 12 Inches']
+const wei = ['1 Pound = 16 Ounces', '1 Kilometer = 2.205 Pounds', '1 Ton = 2000 Pounds']
+
 const ConversionPage = (props) => {
 
-    const {currentList, setCurrentList} = useState() 
-
-    const vol = ['1 Gallon = 4 Quarts', '1 Gallon = 3.785 Liters', '1 Quart = 4 cups', '1 cup = 8 Fluid Ounces']
-    const len = ['1 Mile = 1.609 Kilometers', '1 mile = 1760 Yards', '1 Yard = 3 Feet', '1 Foot = 12 Inches']
-    const wei = ['1 Pound = 16 Ounces', '1 Kilometer = 2.205 Pounds', '1 Ton = 2000 Pounds']
+    const [currentList, setCurrentList] = useState(wei) 
 
     useEffect(()=>{
-        console.log(props.currentUnit)
+        if(props.currentUnit === 'Volume'){
+            setCurrentList(vol)
+        }
+        else if(props.currentUnit === 'Length'){
+            setCurrentList(len)
+        }
+        else if(props.currentUnit === 'Weight'){
+            setCurrentList(wei)
+        }
     }, [props.currentUnit])
 
     return(
         <div class='conver'>
             <Table striped>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Sample</td>
-                    </tr>
+                        
+                        {currentList.map((item) =>{
+                            return(
+                                <tr>
+                                    <td align='center'>{item}</td>
+                                </tr>
+                        )})}  
                 </tbody>
             </Table>
         </div>
